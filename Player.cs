09c1ee5e07e06fd373f659c852c0a82c3edb58d3
Edgar Rodríguez <104480706+EdgarJRM@users.noreply.github.com>
2025-02-15 08:19:59 -0500

    player

--- Player.cs
+++ Player.cs
@@ -1,15 +1,24 @@
 using Godot;
 using System;
-using System.Diagnostics;
 
 public partial class Player : CharacterBody2D
 {
-	private const float SPEED = 400.0f;
+    private const float SPEED = 400.0f;
 
-	public override void _PhysicsProcess(double delta){
-		double direction = Input.GetAxis("ui_up", "ui_down", );
-		Velocity = new Vector2(Velocity.x, direction * SPEED);
+    public override void _PhysicsProcess(double delta)
+    {
+        // Obtener la entrada de los controles (teclas) izquierda/derecha
+        float direction = Input.GetAxis("up", "down");
 
-		MoveAndSlide();
-	}
+        // Modificar la velocidad en el eje Y, pero dejar la velocidad en el eje X intacta
+        Velocity = new Vector2(0, direction * SPEED);
+
+        // Mover al jugador con la velocidad modificada
+        MoveAndSlide();
+    }
+
+    public override void _Ready()
+    {
+        GD.Print("Hola mundo");
+    }
 }
